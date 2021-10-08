51c8a2e7a64d9cc9780ee4b8d13ad061bdf719ab
djreyab <djreya.boyd@gmail.com>
2021-10-08 16:42:15 -0400

    created database for app

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
-from flask import Flask
+# import libraries
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+# initialize database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+#create table structure
+class Todo(db.Model):
+    # __tablename__ = 'todo'
+    usr_id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow) 
+
+
+    def __repr__(self):
+        '''
+        Returns task and id when new task is created.
+        '''
+        return '<Task %r>' % self.id
+
+# create an index route
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
